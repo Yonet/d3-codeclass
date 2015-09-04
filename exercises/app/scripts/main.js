a6ccb8e6f46d188206bf7265b34c4c17fd38868e
Aysegul Yonet <aysegulyonet@gmail.com>
2015-09-04 16:19:30 -0700

    Basic world map added.

--- exercises/app/scripts/main.js
+++ exercises/app/scripts/main.js
@@ -1,2 +1,12 @@
-// jshint devel:true
-console.log('\'Allo \'Allo!');
+d3.json('data/world.geojson', createMap);
+
+function createMap(countries) {
+	console.log('this is running', countries);
+	var aProjection = d3.geo.mercator();
+	var geoPath = d3.geo.path().projection(aProjection);
+	d3.select('svg').selectAll('path').data(countries.features)
+		.enter()
+		.append('path')
+		.attr('d', geoPath)
+		.attr('class',  'countries');
+}
