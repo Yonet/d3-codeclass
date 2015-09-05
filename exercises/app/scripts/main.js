4a538ab8c3e8a7b2a64d9f18c8d24d4ab9f4f6af
Aysegul Yonet <aysegulyonet@gmail.com>
2015-09-04 17:54:27 -0700

    Map outline added.

--- exercises/app/scripts/main.js
+++ exercises/app/scripts/main.js
@@ -2,7 +2,11 @@ d3.json('data/world.geojson', createMap);
 
 function createMap(countries) {
 	console.log('this is running', countries);
+
+	// Each projection has .transform() and .scale() but differs in implementation
 	var aProjection = d3.geo.mercator();
+
+	// d3.geo.path() defaults to alverUSA
 	var geoPath = d3.geo.path().projection(aProjection);
 	d3.select('svg').selectAll('path').data(countries.features)
 		.enter()
