a3617ee1ca398e610a6212a20c5a5186fcf090dc
Aysegul Yonet <aysegulyonet@gmail.com>
2015-09-13 19:31:56 -0700

    Map examples.

--- exercises/app/scripts/usaMap.js
+++ exercises/app/scripts/usaMap.js
@@ -18,47 +18,47 @@ var svg = d3.select(".container").append('svg')
 		height: height
 	});
 
-Get the data
-d3.json('data/states.topo.json', function(error, states){
-	if(error) { 
-		console.log('error'); 
-	} else {
-		console.log('got it', states);
-		svg.selectAll('path')
-			.data(topojson.feature(states, states.objects.states).features)
-			.enter().append('path')
-			.attr('d', path)
-			.style('fill', function(d){ console.log('d',d);return color(d.id)})
-	}
-});
+// Get the data
+// d3.json('data/states.topo.json', function(error, states){
+// 	if(error) { 
+// 		console.log('error'); 
+// 	} else {
+// 		console.log('got it', states);
+// 		svg.selectAll('path')
+// 			.data(topojson.feature(states, states.objects.states).features)
+// 			.enter().append('path')
+// 			.attr('d', path)
+// 			.style('fill', function(d){ console.log('d',d);return color(d.id)})
+// 	}
+// });
 
-// queue()
-// 	.defer(d3.json, 'data/states.topo.json')
-// 	.defer(d3.json, 'data/cities.json')
-// 	.await(makeMyMap);
+queue()
+	.defer(d3.json, 'data/states.topo.json')
+	.defer(d3.json, 'data/cities.json')
+	.await(makeMyMap);
 
 
-// function makeMyMap (error, states, cities) {
-// 	// console.log('city ',cities);
-// 	svg.selectAll('path')
-// 		.data(topojson.feature(states, states.objects.states).features)
-// 		.enter().append('path')
-// 		.attr('d', path)
-// 		.style('fill', function(d){ return color(d.id)})
+function makeMyMap (error, states, cities) {
+	console.log('city ',cities);
+	svg.selectAll('path')
+		.data(topojson.feature(states, states.objects.states).features)
+		.enter().append('path')
+		.attr('d', path)
+		.style('fill', function(d){ return color(d.id)})
 
-// 	//Marks
-// 	// svg.selectAll('.cities')
-// 	// 	.data(cities.features)
-// 	// 	.enter()
-// 	// 	.append('path')
-// 	// 	.attr('d', path.pointRadius(5))
-// 	// 	.attr('class', 'cities');
-// 	// 	
-// 	svg.selectAll('circle').data(cities.features)
-// 		.enter().append('circle')
-// 		.style('fill', 'black')
-// 		.attr('class', 'cities')
-// 		.attr('r', 5)
-// 		.attr('cx', function(d){console.log('d', d.geometry.coordinates);return usaProjection([d.geometry.coordinates[0], d.geometry.coordinates[1]])[0]})
-// 		.attr('cy', function(d){return usaProjection([d.geometry.coordinates[0], d.geometry.coordinates[1]])[1]})
-// }
+	//Marks
+	// svg.selectAll('.cities')
+	// 	.data(cities.features)
+	// 	.enter()
+	// 	.append('path')
+	// 	.attr('d', path.pointRadius(5))
+	// 	.attr('class', 'cities');
+	// 	
+	svg.selectAll('circle').data(cities.features)
+		.enter().append('circle')
+		.style('fill', 'black')
+		.attr('class', 'cities')
+		.attr('r', 5)
+		.attr('cx', function(d){console.log('d', d.geometry.coordinates);return usaProjection([d.geometry.coordinates[0], d.geometry.coordinates[1]])[0]})
+		.attr('cy', function(d){return usaProjection([d.geometry.coordinates[0], d.geometry.coordinates[1]])[1]})
+}
